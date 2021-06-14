e811553be7b0f1b5c3b08b385f0708db6f976c7b
Pierre Mesure <pierre@mesu.re>
2021-06-14 15:24:08 +0200

    Fix the files

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from service.vmd import VMD
 import scrapers.vgr as vgr
 from service.scraper import Scraper
 
-Scraper.scrape_and_write_centers()
-#VMD.convert('14')
+#Scraper.scrape_and_write_centers()
+VMD.convert('14')
